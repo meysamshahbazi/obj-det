cc2328e3dcf36aa525af95c17720498f61096d8f
meysam <mysm.shahbazi@gmail.com>
2022-04-19 09:02:21 +0430

    first commit

---
+++ run.sh
@@ -0,0 +1,4 @@
+
+
+./obd --zoo=../models.yml --model=../yolov4-tiny.weights --config=../yolov4-tiny.cfg  --classes=.^Cobject_detection_classes_yolov3.txt --nms=0.4 --thr=0.5 --height=416 --width=416 --scale=.00392 --mean=0  --backend=5 --target=6 -i=/media/meysam/hdd/Fajr/4.avi
+
